9edb193e54dc8dca2be2c9f2f37a4b3889ea622a
giacomo9999 <gmarchesi888@gmail.com>
2021-06-22 17:04:25 -0400

    Moving forward.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("JS here...");
+const boxesContainer = document.getElementById("boxes");
+const btn = document.getElementById("btn");
+
+const createBoxes = () => {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement("div");
+      console.log(box);
+    }
+  }
+};
+
+createBoxes();
