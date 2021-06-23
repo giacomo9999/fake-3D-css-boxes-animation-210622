88fe7d872990099a8f7215919b8428514fd80590
giacomo9999 <gmarchesi888@gmail.com>
2021-06-23 14:36:30 -0400

    Edited README.

---
+++ README.md
@@ -0,0 +1,3 @@
+Animated cheesy fake-3D box thing that looks like this:  
+
+![Screen Shot 2021-06-22 at 5 26 58 PM](https://user-images.githubusercontent.com/31168329/123150362-45d46c00-d430-11eb-9de5-3f25dc8ed2e2.png)
--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const boxesContainer = document.getElementById("boxes");
 const btn = document.getElementById("btn");
 
+btn.addEventListener("click", () => boxesContainer.classList.toggle("big"));
+
 const createBoxes = () => {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       const box = document.createElement("div");
-      console.log(box);
+      box.classList.add("box");
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      boxesContainer.appendChild(box);
     }
   }
 };
